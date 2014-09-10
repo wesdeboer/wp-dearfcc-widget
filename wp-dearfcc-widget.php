98d5b1a8643f2b076199159b3a85700d433117c4
Wes DeBoer <wesdeboer@gmail.com>
2014-09-09 22:14:16 -0700

    initial commit

---
+++ wp-dearfcc-widget.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Plugin Name: Dear FCC
+ * Description: Embed a widget defending net neutrality by the eff.org (https://www.eff.org/deeplinks/2014/09/reddit-pornhub-websites-slow-down-net-neutrality-september-10)
+ */
+
+class Dear_FCC_Widget {
+
+	static $slowdown_day = '2014-09-10';
+
+	static $options = array(
+			'animation' => 'banner',
+			'theme' => 'dark',
+	);
+
+	function is_slowdown_day() {
+		return (date('Y-m-d') == self::$slowdown_day);
+	}
+
+	function enqueue_scripts() {
+		if (self::is_slowdown_day()) {
+			wp_enqueue_script( 'dearfcc', 'https://s.eff.org/dearfcc-widget/widget.min.js', array(), null, true );
+			wp_localize_script( 'dearfcc', '_bftn_options', self::$options );
+		}
+	}
+}
+
+add_action( 'wp_enqueue_scripts', array( 'Dear_FCC_Widget', 'enqueue_scripts' ), 2 );
